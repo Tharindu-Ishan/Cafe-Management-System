90558aa99367eb5b91e758e89daf73618b4b5912
Tharindu Ishan <ishanthari96@gmail.com>
2023-12-30 19:10:36 +0530

    Generate report api of bill

--- back-end/src/main/java/lk/ijse/dep11/restImpl/BillRestImpl.java
+++ back-end/src/main/java/lk/ijse/dep11/restImpl/BillRestImpl.java
@@ -1,8 +1,27 @@
 package lk.ijse.dep11.restImpl;
 
+import lk.ijse.dep11.constants.CafeConstants;
 import lk.ijse.dep11.rest.BillRest;
+import lk.ijse.dep11.service.BillService;
+import lk.ijse.dep11.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class BillRestImpl implements BillRest {
+    @Autowired
+    BillService billService;
+    @Override
+    public ResponseEntity<String> generateReport(Map<String, Object> requestMap) {
+        try {
+            return billService.generateReport(requestMap);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
