27ff7ffd8ca4d666f7c65a8057e15fd7203a4a09
tharindu ishan <ishanthari96@gmail.com>
2024-01-25 16:13:48 +0530

    Make order page

--- cafe-springboot-angular-frontend/Frontend/src/app/material-component/material.routing.ts
+++ cafe-springboot-angular-frontend/Frontend/src/app/material-component/material.routing.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
 import { RouteGuardService } from '../services/route-guard.service';
+import { ManageProductComponent } from './manage-product/manage-product.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
 
 
 export const MaterialRoutes: Routes = [
@@ -11,6 +13,22 @@ export const MaterialRoutes: Routes = [
         canActivate:[RouteGuardService],
         data:{
             exportedRole:['admin']
+        }
+    },
+    {
+        path:'product',
+        component:ManageProductComponent,
+        canActivate:[RouteGuardService],
+        data:{
+            exportedRole:['admin']
+        }
+    },
+    {
+        path:'order',
+        component:ManageOrderComponent,
+        canActivate:[RouteGuardService],
+        data:{
+            exportedRole:['admin','user']
         }
     }
 ];
